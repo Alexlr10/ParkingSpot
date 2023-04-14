3c8676d1d55616b6fc5eb72841349ecd41a709e9
Alexlr10 <alexlr10@hotmail.com>
2023-04-14 16:49:10 -0300

    inserindo spring security

--- src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "TB_PARKING_SPOT")
 public class ParkingSpotModel implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
